518488053b8593ba6d32115d3ab417f85184f048
Betancur Mariano <125041706+MDB-Xeneize@users.noreply.github.com>
2023-08-17 00:05:25 -0300

    Update backend.sql

---
+++ Proyecto/backend.sql
@@ -0,0 +1,15 @@
+create database backend;
+create table persona(
+ dni INT primary key,
+ nombre VARCHAR(30) NOT NULL,
+ apellido VARCHAR(30) NOT NULL
+ );
+ 
+ create table usuario(
+ mail VARCHAR(40)  primary key,
+ nickname VARCHAR(30) NOT NULL,
+ password VARCHAR(30) NOT NULL
+ );
+ 
+ 
+ ALTER TABLE usuario ADD COLUMN persona_dni INT, ADD FOREIGN KEY (persona_dni) REFERENCES persona(dni);
